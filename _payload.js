9e48cd353fa0f86220b61bfeb62597a72111ffe0
phi-friday <phi-friday@users.noreply.github.com>
2023-04-05 16:37:37 +0000

    deploy: 2e0ccd032b8bdb97c44a9d57972b03149ec13e32

--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{"last-article-only-path":{_path:"\u002Fpython\u002Freturns2"}},prerenderedAt:1680712244596}
+export default {data:{"last-article-only-path":{_path:"\u002Fpython\u002Freturns2"}},prerenderedAt:1680712653866}
