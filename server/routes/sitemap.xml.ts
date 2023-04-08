6be18bea1b647b7c3cd6a3f4f247aa2e76ef1dd4
phi <phi.friday@gmail.com>
2023-04-09 07:00:10 +0900

    fix: sitemap index 사용

--- server/routes/sitemap.xml.ts
+++ server/routes/sitemap.xml.ts
@@ -1,4 +1,4 @@
-import { SitemapStream, streamToPromise } from 'sitemap';
+import { SitemapAndIndexStream, SitemapStream, streamToPromise } from 'sitemap';
 import { dirname, resolve } from 'path';
 
 import { ParsedContent } from '@nuxt/content/dist/runtime/types';
@@ -26,38 +26,9 @@ const get_date = (doc: ParsedContent) => {
   return (doc.date as string).match(re_date)?.at(0) as string;
 };
 
-export default defineEventHandler(async (event) => {
-  const sitemap = new SitemapStream({ hostname: BASE_URL });
-
-  const docs = await serverQueryContent(event)
-    .where({ publish: { $eq: true }, title: { $ne: 'dummy' } })
-    .find();
-  let date: string | undefined;
-  for (const doc of docs) {
-    date = get_date(doc);
-    if (date) {
-      sitemap.write({
-        url: add_prefix(doc._path),
-        changefreq: 'daily',
-        lastmod: doc.date,
-      });
-    } else {
-      sitemap.write({ url: add_prefix(doc._path), changefreq: 'daily' });
-    }
-  }
-
-  const staticEndpoints = getStaticEndpoints();
-  for (const staticEndpoint of staticEndpoints) {
-    sitemap.write({ url: staticEndpoint, changefreq: 'daily' });
-  }
-
-  sitemap.end();
-  return streamToPromise(sitemap);
-});
-
-function getStaticEndpoints(): string[] {
+const get_static_endpoints = (): string[] => {
   const __dirname = dirname(fileURLToPath(import.meta.url));
-  const files = getFiles(`${__dirname}/../../pages`);
+  const files = get_files(`${__dirname}/../../pages`);
   return files
     .filter((file) => !file.includes('slug')) // exclude dynamic content
     .map((file) => file.split('pages')[1])
@@ -66,16 +37,73 @@ function getStaticEndpoints(): string[] {
         ? file.split('/index.vue')[0]
         : file.split('.vue')[0];
     });
-}
+};
 
 /**
  * recursively get all files from /pages folder
  */
-function getFiles(dir: string): string[] {
+const get_files = (dir: string): string[] => {
   const dirents = fs.readdirSync(dir, { withFileTypes: true });
   const files = dirents.map((dirent) => {
     const res = resolve(dir, dirent.name);
-    return dirent.isDirectory() ? getFiles(res) : res;
+    return dirent.isDirectory() ? get_files(res) : res;
   });
   return files.flat();
-}
+};
+
+const xml_files: Set<string> = new Set();
+
+export default defineEventHandler(async (event) => {
+  const sitemap_index_stream = new SitemapAndIndexStream({
+    limit: 45000,
+    lastmodDateOnly: false,
+    getSitemapStream: (i: number) => {
+      const sitemap_stream = new SitemapStream({ hostname: BASE_URL });
+      const path = `./sitemap-${i}.xml`;
+      xml_files.add(path);
+      const ws = sitemap_stream.pipe(fs.createWriteStream(resolve(path)));
+
+      return [new URL(path, BASE_URL).toString(), sitemap_stream, ws];
+    },
+  });
+
+  const docs = await serverQueryContent(event)
+    .where({ publish: { $eq: true }, title: { $ne: 'dummy' } })
+    .find();
+  let date: string | undefined;
+  for (const doc of docs) {
+    date = get_date(doc);
+    if (date) {
+      sitemap_index_stream.write({
+        url: add_prefix(doc._path),
+        changefreq: 'daily',
+        lastmod: doc.date,
+      });
+    } else {
+      sitemap_index_stream.write({ url: add_prefix(doc._path), changefreq: 'daily' });
+    }
+  }
+
+  const static_endpoints = get_static_endpoints();
+  for (const static_endpoint of static_endpoints) {
+    sitemap_index_stream.write({ url: static_endpoint, changefreq: 'daily' });
+  }
+
+  sitemap_index_stream.end();
+  await Promise.all(
+    [...xml_files].map((xml_file) => {
+      return fs.promises.rename(origin_xml_path(xml_file), new_xml_path(xml_file));
+    })
+  );
+  return streamToPromise(sitemap_index_stream);
+});
+
+const origin_xml_path = (xml_file: string) => {
+  const filename = xml_file.split('/').at(-1);
+  return `${process.env.PWD}/${filename}`;
+};
+
+const new_xml_path = (xml_file: string) => {
+  const filename = xml_file.split('/').at(-1);
+  return `${process.env.PWD}/.output/public/${filename}`;
+};
