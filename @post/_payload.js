6bc7d0571b0dc706cf205b60ff6b9dfb921e1212
phi-friday <phi-friday@users.noreply.github.com>
2023-04-08 14:21:07 +0000

    deploy: 06d01322003cf18bcbcfde6901636f9d883738b5

--- @post/_payload.js
+++ @post/_payload.js
@@ -1,1 +1,1 @@
-export default {data:{},prerenderedAt:1680963034059}
+export default {data:{},prerenderedAt:1680963665879}
--- _payload.js
+++ _payload.js
@@ -1,1 +1,1 @@
-export default {data:{"last-article-only-path":{_path:"\u002Fjs\u002Fnextjs-nuxtjs"}},prerenderedAt:1680963034126}
+export default {data:{"last-article-only-path":{_path:"\u002Fjs\u002Fnextjs-nuxtjs"}},prerenderedAt:1680963665963}
