3933a10dbaa5c9bc1095e23b90dd2884648d605a
phi <phi.friday@gmail.com>
2023-04-08 23:26:59 +0900

    fix: remove robots disallow

--- robots.config.js
+++ robots.config.js
@@ -1,6 +1,5 @@
 export default [
   { UserAgent: '*' },
-  { Disallow: '/' },
   { BlankLine: true },
   // Be aware that this will NOT work on target: 'static' mode
   { Sitemap: (req) => `${process.env.NUXT_HOSTNAME}/sitemap.xml` }
