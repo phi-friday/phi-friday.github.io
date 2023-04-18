35775fa5b158c81eeefe823349415a9e2035e277
phi <phi.friday@gmail.com>
2023-04-18 23:34:53 +0900

    fix: sitemap meta

--- app.vue
+++ app.vue
@@ -22,6 +22,7 @@ useHead({
     { name: 'google-site-verification', content: config.public.google.seo },
   ],
   link: [
+    { rel: 'sitemap', type: 'application/xml', title: 'Sitemap', href: '/sitemap.xml' },
     { rel: 'apple-touch-icon', sizes: '180x180', href: '/apple-touch-icon.png' },
     { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
     { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
