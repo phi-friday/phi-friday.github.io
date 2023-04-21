f5932d45fce838b28bfd2463f9fc01162a270554
phi <phi.friday@gmail.com>
2023-04-22 00:34:26 +0900

    fix: ProseP override(leading-7)

---
+++ components/content/prose_p.vue
@@ -0,0 +1,20 @@
+<template>
+  <p class="leading-7"><slot /></p>
+</template>
+
+<style lang="ts" scoped>
+// @ts-expect-error
+css({
+  p: {
+    fontFamily: '{typography.font.body}',
+    fontSize: '{prose.p.fontSize}',
+    // lineHeight: '{prose.p.lineHeight}',
+    margin: '{prose.p.margin}',
+    ':deep(br)': {
+      display: "block",
+      content: "''",
+      margin: '{prose.p.br.margin}'
+    }
+  }
+})
+</style>
