e31567b8af3ac5c5f3298f87c9c7dd7144ce3d40
phi <phi.friday@gmail.com>
2023-04-23 03:50:24 +0900

    fix: robots add disallows

--- robots.config.js
+++ robots.config.js
@@ -1,6 +1,10 @@
 export default [
   { UserAgent: '*' },
   { BlankLine: true },
+  { Allow: '*' },
+  { Disallow: '/' },
+  { Disallow: '/@post' },
+  { Disallow: '/@post/' },
   // Be aware that this will NOT work on target: 'static' mode
-  { Sitemap: (req) => `${process.env.NUXT_HOSTNAME}/sitemap.xml` }
-]
+  { Sitemap: (req) => `${process.env.NUXT_HOSTNAME}/sitemap.xml` },
+];
