7fefea35657817a955383e4f9642e3f306a81394
phi <phi.friday@gmail.com>
2023-04-23 03:56:08 +0900

    fix: add allows

--- robots.config.js
+++ robots.config.js
@@ -4,7 +4,13 @@ export default [
   { Disallow: '/' },
   { Disallow: '/@post' },
   { Disallow: '/@post/' },
-  { Allow: '*' },
+  { Allow: '/@post/*' },
+  { Allow: '/@tag' },
+  { Allow: '/@tag/' },
+  { Allow: '/@tag/*' },
+  { Allow: '/@page' },
+  { Allow: '/@page/' },
+  { Allow: '/@page/*' },
   // Be aware that this will NOT work on target: 'static' mode
   { Sitemap: (req) => `${process.env.NUXT_HOSTNAME}/sitemap.xml` },
 ];
