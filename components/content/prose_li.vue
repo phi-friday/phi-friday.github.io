bdbeb25516c3b85f0047f585324a19ab168c8a3b
phi <phi.friday@gmail.com>
2023-04-25 00:36:26 +0900

    fix: li도 leading-7 적용

---
+++ components/content/prose_li.vue
@@ -0,0 +1,13 @@
+<template>
+  <li class="leading-7"><slot /></li>
+</template>
+
+<style lang="ts" scoped>
+css({
+  li: {
+    fontFamily: '{typography.font.body}',
+    margin: '{prose.li.margin}',
+    listStylePosition: '{prose.li.listStylePosition}',
+  }
+})
+</style>
