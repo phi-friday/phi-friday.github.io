e9df791e320ffc40de9b1fe70e4685afcfc54ba6
phi <phi.friday@gmail.com>
2023-08-02 02:18:09 +0900

    fix: add alert outer

---
+++ components/content/alert_outer.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="alert_outer">
+    <Alert :type="type"><slot /></Alert>
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps({
+  type: {
+    type: String,
+    default: 'info',
+    validator(value: string) {
+      return ['info', 'success', 'warning', 'danger', 'primary'].includes(value);
+    },
+  },
+});
+</script>
+
+<style scoped lang="css">
+.alert_outer > .alert {
+  @apply my-1 py-1;
+}
+</style>
