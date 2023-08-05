4d97baa4d84c4f2921a6bee42b475bee8db4afc3
phi <phi.friday@gmail.com>
2023-08-05 22:24:00 +0900

    fix: remove footer in root path

--- components/site_footer.vue
+++ components/site_footer.vue
@@ -1,13 +1,17 @@
 <template>
-  <footer class="site-footer">
+  <footer v-if="!root_path" class="site-footer">
     <div class="wrapper">
-      <div class="footer-info">
-      </div>
-      <ul class="footer-links">
-      </ul>
+      <div class="footer-info"></div>
+      <ul class="footer-links"></ul>
     </div>
   </footer>
 </template>
+
+<script setup lang="ts">
+const route = useRoute();
+const root_path = computed(() => route.path === '/');
+</script>
+
 <style scoped lang="css">
 .site-footer {
   @apply px-4 py-12 border-t border-slate-100 dark:border-neutral-800;
