4e2f053d9a5c90f3b3f48f0acd18d25b063e0ccc
phi <phi.friday@gmail.com>
2026-04-04 20:33:39 +0900

    fix: post

--- src/routes/(app)/@post/[...slug]/+page.svelte
+++ src/routes/(app)/@post/[...slug]/+page.svelte
@@ -1,25 +1,17 @@
 <script lang="ts">
-  import type { Component } from "svelte";
+  import type { ContentFile } from "$lib/utils/contents";
 
   import Comments from "$lib/components/Comments.svelte";
-  import Loading from "$lib/components/Loading.svelte";
   import PostNavigation from "$lib/components/post/PostNavigation.svelte";
 
   import type { PageProps } from "./$types";
 
   let { data }: PageProps = $props();
 
-  const ComponentPromise: Promise<Component> | undefined = $derived(
-    // oxlint-disable-next-line unicorn/prefer-top-level-await
-    Object.entries(import.meta.glob("../../../../contents/**/*.{md,svx}"))
+  const Post = $derived(
+    await Object.entries(import.meta.glob<ContentFile>("../../../../contents/**/*.{md,svx}"))
       .find(([path]) => path.endsWith(`${data.slug}.${data.ext}`))?.[1]?.()
-      // @ts-expect-error: dynamic import
       .then(mod => mod.default)
-      .catch((error: unknown) => {
-        // oxlint-disable-next-line no-console: FIXME
-        console.error(`Failed to load content for slug: ${data.slug}.${data.ext}`, error);
-        throw error;
-      })
   );
 
   const page_title = $derived(`${data.metadata.title} | ${import.meta.env.APP_NAME}`);
@@ -36,12 +28,6 @@
   {/if}
 </svelte:head>
 
-{#await ComponentPromise}
-  <Loading />
-{:then LoadedComponent}
-  <LoadedComponent />
-  <PostNavigation prev={data.prev} next={data.next} />
-  <Comments />
-{:catch error}
-  <p class="text-red-500">Failed to load content: {error.message}</p>
-{/await}
+<Post />
+<PostNavigation prev={data.prev} next={data.next} />
+<Comments />
